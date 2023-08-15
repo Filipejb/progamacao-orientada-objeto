894678f8599c2cbeca73834dd3b3482c7a3c9de5
Filipejb <127758798+Filipejb@users.noreply.github.com>
2023-08-14 23:43:58 -0300

    Update verificaNumeroPrimo.java

--- atividade 1/verificaNumeroPrimo.java
+++ atividade 1/verificaNumeroPrimo.java
@@ -1,1 +1,26 @@
+package primos;
 
+import java.until.Scanner;
+
+public class numeroprimo {
+  public static void main(string[] args){
+    
+    scanner leitor = new scanner(sustem.in);
+    int numprimo;
+    int numdivi = 0;
+    
+    system.out.println("digite um numero");
+    numprimo = leitor.nextint();
+    
+    for(int i = 1; i <= numprimo; i++){
+      if(numprimo % i == 0){
+        numdivi++;
+      }
+    }
+    if(numdivi == 2){
+      system.out.println("1");
+    }esle {
+      system.out.println("0");
+    }
+  }
+}
